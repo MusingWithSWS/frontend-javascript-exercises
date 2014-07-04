b067a9baec1734205f4fb0fafc9ba4884f6c9dbc
Christina Bennett <xAfirelove@aol.com>
2014-07-04 17:16:16 -0400

    modified challenge file

--- 02-reading-mocha-tests/00-a-tested-function/challenge.js
+++ 02-reading-mocha-tests/00-a-tested-function/challenge.js
@@ -1,12 +1,3 @@
 module.exports.linkTo = function(text, address) {   
   return "<a href='" + address + "'>" + text + "</a>"
-<<<<<<< HEAD
 };
-=======
-};
-
-returns the URL and the link name
-<<<<<<< HEAD
->>>>>>> 972d4d98c626067219f93c6d460bd30528827184
-=======
->>>>>>> 972d4d98c626067219f93c6d460bd30528827184
